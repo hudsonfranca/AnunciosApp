52490889a99a9860d98229c15a5492d866fd2293
hudsonfranca <hudsonsilvares@gmail.com>
2021-01-28 20:09:45 -0300

    frontend first commit

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Head from 'next/head'
+
+import { Container } from '../styles/pages/Home'
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      <h1>FRONTEND 2</h1>
+    </Container>
+  )
+}
+
+export default Home
