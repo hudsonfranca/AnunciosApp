8cfe45e6e0006aa261948c2e044fcfa1c3bbe991
hudsonfranca <hudsonsilvares@gmail.com>
2021-03-02 22:09:34 -0300

    adding persistent volume to the backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import{keys} from './keys'
 
 
 async function bootstrap() {
-
+console.log(keys)
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.useGlobalPipes(
